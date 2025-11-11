a241be3a815071f54c4c132161d7319568e85caf
Roslin Mahmud Joy <roslin.joy@brainstation-23.com>
2025-11-11 17:03:25 +0200

    Update root endpoint to return HTML response

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import os
+from fastapi.responses import HTMLResponse
 import uvicorn
 
 @asynccontextmanager
@@ -13,7 +14,14 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello from FastAPI todo-app"}
+    html = """
+    <!doctype html>
+    <html>
+      <head><title>Todo App</title></head>
+      <body><h1>Hello from FastAPI todo-app</h1></body>
+    </html>
+    """
+    return HTMLResponse(content=html, status_code=200)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
