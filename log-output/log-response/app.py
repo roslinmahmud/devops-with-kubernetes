6b18f3dd4cd80512d823ba103f6f8525252e108c
Roslin Mahmud Joy <roslin.joy@brainstation-23.com>
2025-12-23 17:21:08 +0200

    feat: add ConfigMap for MESSAGE env variable and information.txt, update deployment to use it

--- log-output/log-response/app.py
+++ log-output/log-response/app.py
@@ -1,5 +1,4 @@
 import os
-from datetime import datetime, UTC
 from fastapi import FastAPI
 import uvicorn
 from fastapi.responses import HTMLResponse
@@ -9,16 +8,26 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
+    # Read from information.txt
+    with open("/etc/config/information.txt", "r") as f:
+        file_content = f.read().strip()
+
+    # Get MESSAGE env variable
+    message = os.getenv("MESSAGE", "default message")
+
     # read from log.txt the content
     with open("files/log.txt", "r") as f:
         lines = f.readlines()
+
     # # read from counter.txt the content
     # with open("files/counter.txt", "r") as f:
     #     counter_value = f.read().strip()
+
     async with AsyncClient() as client:
         response = await client.get("http://pingpong-svc:2346/pings")
         counter_value = response.json().get("counter", 0)
-    content = f"{lines[-1].rstrip()}<br/>Ping / Pongs: {counter_value}"
+
+    content = f"file content: {file_content}<br/>env variable: MESSAGE={message}<br/>{lines[-1].rstrip()}<br/>Ping / Pongs: {counter_value}"
     return HTMLResponse(content=content, media_type="text/html")
 
 @app.get("/status")
